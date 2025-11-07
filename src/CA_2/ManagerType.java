0a6d66789ab6a03f3a5710b5161ba13e19a829e4
yagog <yagog@Administrator>
2025-11-07 16:21:46 +0000

    3 commit - creating some java file, because each java is going to have a specific funtion in the project and put in the same time main java as a head structure of this poroject code

---
+++ src/CA_2/ManagerType.java
@@ -0,0 +1,14 @@
+package CA_2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author yagog
+ */
+public enum ManagerType {
+    
+}
