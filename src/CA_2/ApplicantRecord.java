7a1d7d321a1584cdea7ff9d2caf1bb836a9ae0cc
yagog <yagog@Administrator>
2025-11-14 10:57:28 +0000

    6 commit - persistence + io basics + creating txt (put staff for test) staff repo saves managers/employees; file service loads applicants from CSV ; ready for sorting.

--- src/CA_2/ApplicantRecord.java
+++ src/CA_2/ApplicantRecord.java
@@ -10,4 +10,33 @@ package CA_2;
  */
 public class ApplicantRecord {
     
+    private String fullname;
+    private ManagerType managerType;
+    private DepartmentType departmentType;
+    
+    public ApplicantRecord(String fullname, ManagerType managerType, DepartmentType departmentType) {
+        this.fullname = fullname;
+        this.managerType = managerType;
+        this.departmentType = departmentType;
+    }
+    
+    public String getfullname() {
+        return fullname;
+    }
+    
+    public ManagerType getmanagerType() {
+        return managerType;
+    }
+    
+    public DepartmentType getdepartmentType() {
+        return departmentType;
+    }
+    
+    @Override
+    public String toString() {
+        return "ApplicantRecord {" + "Full Name='" + fullname + '\'' + ", Manager Type=" + managerType + ", Department Type=" + departmentType + '}';
+    }
+    
+    
+    
 }
--- src/CA_2/FileService.java
+++ src/CA_2/FileService.java
@@ -4,10 +4,37 @@
  */
 package CA_2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author yagog
  */
 public class FileService {
-    
+    public static List<ApplicantRecord> readApplicantsFromFile(String filePath) {
+        List<ApplicantRecord> applicants = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            br.readLine(); // This code will skip header
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+                if (parts.length == 3) {
+                    String fullname = parts[0].trim();
+                    ManagerType managerType = ManagerType.valueOf(parts[1].trim().toUpperCase());
+                    DepartmentType departmentType = DepartmentType.valueOf(parts[2].trim().toUpperCase());
+                    applicants.add(new ApplicantRecord(fullname, managerType, departmentType));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error to read the file " + e.getMessage());
+        }
+
+        return applicants;
+    } 
 }
